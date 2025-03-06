c3398aefa1f979ed4b50edfb4f9e238b699609e9
Lee <152392640+web-developer-lee@users.noreply.github.com>
2025-03-06 12:27:13 +0000

    Plugin updater setup

---
+++ vivid-voting.php
@@ -0,0 +1,28 @@
+<?php
+    /**
+     * The plugin bootstrap file
+     *
+     * @link              https://vivideverywhere.co.uk/
+     * @since             1.0.0
+     * @package           Vivid Voting
+     *
+     * @wordpress-plugin
+     * Plugin Name:       Vivid Voting
+     * Plugin URI:        https://plugins.vivideverywhere.co.uk/voting/
+     * Description:       Custom voting system for WordPress
+     * Version:           1.0.000
+     * Author:            Vivid Everywhere
+     * Author URI:        https://vivideverywhere.co.uk/
+     * License:           MIT
+     * License URI:       https://opensource.org/licenses/MIT
+     * Text Domain:       vivid-voting
+     * Domain Path:       /languages
+     */
+
+// If this file is called directly, abort.
+    if (!defined('WPINC')) {
+        die;
+    }
+
+    require plugin_dir_path(__FILE__) . 'vendor/autoload.php';
+    new VividVoting\Updater();
